dbb6aa896a723fba7a00e9a4a85549b31c36e53c
kimyoda <141495978+kimyoda@users.noreply.github.com>
2024-12-29 16:06:51 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.js
@@ -0,0 +1,8 @@
+// 1. 공백 한개로 구분되어있는 문자열 my_string
+// 2. 앞에서부터 순서대로 담은 문자열 배열 return
+function solution(my_string) {
+    // 1. 단어의 공백을 분리하고 단어를 순서대로 담는다.
+    const words = my_string.split(" ");
+    
+    return words;
+}
