94e3fd847aaea8ca5ae9c72c3130379c7ecf3667
kimyoda <141495978+kimyoda@users.noreply.github.com>
2024-12-30 00:57:56 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 26.68 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,21 +1,35 @@
-// 정수 배열 arr의 값이 50보다 크거나 짝수면 2로 나눈다.
-// 50보다 작은 홀수면 2를 곱한다.
-// 그 결과인 정수 배열을 리턴한다.
-function solution(arr) {
-    const transformedSequence = [];
-    // 반복문을 돌려 arr[i]가 
-    for (let i = 0; i < arr.length; i++) {
-      // 50보다 크거나 같은 짝수면 2로 나누고
-      if (arr[i] >= 50 && arr[i] % 2 === 0) {
-        transformedSequence.push(arr[i] / 2);
-      // 50보다 작거나 홀수면 2를 곱한다.
-      } else if (arr[i] < 50 && arr[i] % 2 !== 0) {
-        transformedSequence.push(arr[i] * 2);
-      } else {
-      // 그 외에 값은 추가한다.
-        transformedSequence.push(arr[i]);
-      }
-    }
+// // 정수 배열 arr의 값이 50보다 크거나 짝수면 2로 나눈다.
+// // 50보다 작은 홀수면 2를 곱한다.
+// // 그 결과인 정수 배열을 리턴한다.
+// function solution(arr) {
+//     const transformedSequence = [];
+//     // 반복문을 돌려 arr[i]가 
+//     for (let i = 0; i < arr.length; i++) {
+//       const value = arr[i];
+
+//       // 50보다 크거나 같은 짝수면 2로 나누고
+//       if (value >= 50 && value % 2 === 0) {
+//         transformedSequence.push(value / 2);
+//       // 50보다 작거나 홀수면 2를 곱한다.
+//       } else if (value < 50 && value % 2 !== 0) {
+//         transformedSequence.push(value * 2);
+//       } else {
+//       // 그 외에 값은 추가한다.
+//         transformedSequence.push(value);
+//       }
+//     }
     
-    return transformedSequence;
+//     return transformedSequence;
+// }
+
+function solution(arr) {
+ const transformedValue = arr.map((value) => {
+  if (value >= 50 && value % 2 === 0) {
+    return value / 2; // 50 이상 짝수 일때 나누기
+  } else if (value < 50 && value % 2 !== 0) {
+    return value * 2; // 50 이하 홀수 일때 곱하기
+  } 
+    return value; // 그 외의 값
+ });
+  return transformedValue; // 최종 변환값 반환
 }
