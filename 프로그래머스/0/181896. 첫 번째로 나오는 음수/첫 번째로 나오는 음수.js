8e0baf104254729abcd5d79ab0f45b03eff7b795
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-01 23:07:21 +0900

    Update 첫 번째로 나오는 음수.js

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -3,6 +3,6 @@
 function solution(num_list) {
     // num_list에서 음수가 나오는 인덱스를 리턴
     // 없으면 -1 리턴
-    const answer = num_list.findIndex((num) => num < 0);
-    return answer;
+    const firstNegativeNumber = num_list.findIndex((num) => num < 0);
+    return firstNegativeNumber;
 }
