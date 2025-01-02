1e78aef8f3e2ae2f3d862e7419e3a51f7bd8e9dc
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-03 00:35:41 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -4,9 +4,9 @@
 function solution(my_string, is_prefix) {
     // 1. my_string의 is_prefix의 값이 true인지 false인 지 구하고 그 true면 1 아니면 0을 담아 변수 선언에 값을 할당한다.
     // 2. startsWith라는 메서드를 통해 문자열의 문자로 시작하는 확인 후 결과를 true 혹은 false로 반환한다.
-    // 3. 삼항연산자를 통해서, 1 혹은 0을 true일때와 false일때로 조건을 넣는다.
-    const prefixString = my_string.startsWith(is_prefix) ? 1 : 0;
-    return prefixString;
+    // 3. +단항연산자를 활용해 1과 0을 리턴한다.
+    const hasPrefix = my_string.startsWith(is_prefix);
+    return +hasPrefix;
     
     // 2번째 풀이(slice 활용)
     // 0번 인덱스부터 is_prefix의 길이만큼 추출한 값이 
