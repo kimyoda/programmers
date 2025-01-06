a116812ca34109d6c067a9c5988c9fed2c4804bd
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-06 20:35:11 +0900

    Update 최댓값 만들기 （2）.js
    이중for문으로 문제수정

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -2,11 +2,13 @@
 // numbers 원소 중 두개를 곱해 최댓값 구하기
 function solution(numbers) {
     let maxProduct = -Infinity;
-
+    // 이중 for문 사용, 모든 두 수의 조합을 사용한다.
     for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            maxProduct = Math.max(maxProduct, numbers[i] * numbers[j]);
-        }
+    // 처음 i는 i 다음 인덱스부터 시작하여, j가 i와 같지않게 1씩 더하여 반복한 값을 numbers에 각각 담아 최소, 최대값을 곱한다. 
+      for (let j = i + 1; j < numbers.length; j++) {
+        //  현재 최대값과 두 수의 곱을 비교하여 더 큰 값을 maxProduct에 할당한다. 
+        maxProduct = Math.max(maxProduct, numbers[i] * numbers[j]);
+      }
     }
 
     return maxProduct;
