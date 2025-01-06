9711bec8c40d751cfbf5376a00c047cc1478880b
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-06 18:27:26 +0900

    Update 뒤에서 5등 위로.js
    불필요한 연산제거

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -8,11 +8,5 @@ function solution(num_list) {
   // copiedNumList중 slice를 활용하여 5번째 인덱스 값이 있는 새로운 배열을 할당하기
   const remainedNumbers = copiedNumList.slice(5);
   
-  // remainNumbers 값 스프레드연산자로 원본배열 담기
-  const copiedRemainedNumbers = [...remainedNumbers];
-    
-  // slice로 인덱스5전까지 잘라진 부분을 제외하고 오름차순으로 값을 새로운 배열로 반환한다.
-  copiedRemainedNumbers.sort((a, b) => a - b);
-    
-  return copiedRemainedNumbers;
+  return remainedNumbers;
 }
