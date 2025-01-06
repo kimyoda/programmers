7d4ec3b668fe09f1ba454784268938f37799c4aa
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-06 17:29:19 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,18 @@
+// 문자열 my_string, 정수 num1, num2
+// my_string 인덱스 num1 과 num2 문자를 바꾼 문자열
+function solution(my_string, num1, num2) {
+    // 문자열을 배열로 만들기(전개구문)
+    const copiedArrStr = [...my_string];
+    
+    // 배열로 만든 값에 num1, num2 교환하기
+    const changedStr = copiedArrStr[num1]; // "e"
+    // copiedArrStr[num1]의 "e"에 copiedArrStr[num2]의 "l"를 할당한다.
+    copiedArrStr[num1] = copiedArrStr[num2];
+    // copiedArrStr[num2]의 "l"에 "e"을 할당한다.
+    copiedArrStr[num2] = changedStr;
+    
+    // join으로 배열을 문자열로 다시 리턴한다
+    const result = copiedArrStr.join('');
+    
+    return result;
+}
