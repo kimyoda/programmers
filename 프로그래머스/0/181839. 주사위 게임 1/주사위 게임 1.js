38d4bbe1d3dc4cd7ed82d30a6b205ab15a9ec0c4
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-07 14:32:38 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,24 @@
+// 두 주사위를 굴렸을 때 나온 숫자 a, b
+// 1. a, b가 모두 홀수면 a(제곱) + b(제곱)점을 얻는다.
+// 2. a, b 중하나가 홀수면 2 x (a+ b)점
+// 3. a, b 모두 홀수가 아니면 |a - b|점을 얻는다.(절대값음수..?)
+function solution(a, b) {
+    let score = 0;
+    // a, b를 변수를 선언하여 할당한다.
+    let firstDiceNumber = a;
+    let secondDiceNumber = b;
+    // 1. a,b가 모두 홀수면 a(제곱) + b(제곱)을 더한 값을 할당한다.
+    if (firstDiceNumber % 2 !== 0 && secondDiceNumber % 2 !== 0) {
+      // 두 변수가 홀수면 제곱을 곱한 값을 더해 score 변수에 할당한다.
+      score = firstDiceNumber ** 2 + secondDiceNumber ** 2;
+    // 2. a, b 중 하나만 홀수라면 2 x (a + b)점을 얻는다.
+    } else if (firstDiceNumber % 2 !== 0 || secondDiceNumber % 2 !== 0) {
+      // or 연산자를 활용하여 두 변수 중 하나만 홀수면 두 변수를 더하고 2를 곱한 값을 할당한다.
+      score = 2 * (firstDiceNumber + secondDiceNumber);
+    // 3. a, b 모두 홀수가 아니면 |a -b| 점을 얻는다.
+    } else if (firstDiceNumber % 2 === 0 && secondDiceNumber % 2 === 0) {
+      // and 연산자를 활용하여 두 변수 모두 홀수가 아닐때, Math.abs 메서드를 활용해서 두 변수의 절대값을 구한다. 
+      score = Math.abs(firstDiceNumber - secondDiceNumber);
+    } 
+    return score;
+}
