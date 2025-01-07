6059c948b98d925512f417d7a78c5171a1e12d21
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-07 15:18:04 +0900

    [level 0] Title: 정수 찾기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
@@ -3,12 +3,12 @@
 function solution(num_list, n) {
     // includes() 메서드를 활용해 특정 값이 포함되어있으면 true또는 false를 반환한다.
     // 이를 삼항연산자로 true, false를 받고 true일 때 1, false일때 0을 할당한다.
-    const arrayFindNumbers = num_list.includes(n) ? 1 : 0;
+    const arrayFindNumber = num_list.includes(n) ? 1 : 0;
     
-    return arrayFindNumbers;
+    return arrayFindNumber;
     
     // filter 써보기
-//     const arrayFindNumbers = num_list.filter((number) => number === n).length > 0 ? 1 : 0;
+//     const arrayFindNumber = num_list.filter((number) => number === n).length > 0 ? 1 : 0;
     
-//     return arrayFindNumbers;
+//     return arrayFindNumber;
 }
