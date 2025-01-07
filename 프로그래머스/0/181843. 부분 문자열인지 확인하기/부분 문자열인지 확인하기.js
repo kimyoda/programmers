c6b0d44adc0a5f2aecfae9cb2aaf2e1735b43a28
kimyoda <rladygks1210@naver.com>
2025-01-07 17:19:25 +0900

    [fix] 변수명 변경(hasPartedStr -> isPartialString)

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -3,8 +3,8 @@
 // true 1 아니면 0을 반환해라
 function solution(my_string, target) {
     // 1. String.includes()를 사용해서 my_string 문자열이 target이 문자열에 포함되어 있는지 확인 후, true일때 1 아닐때 false를 반환하여 변수에 할당한다.
-    const hasPartedStr = my_string.includes(target) ? 1 : 0;
+    const isPartialString = my_string.includes(target) ? 1 : 0;
     
     // 할당한 변수를 리턴하여 종료한다.
-    return hasPartedStr;
+    return isPartialString;
 }
