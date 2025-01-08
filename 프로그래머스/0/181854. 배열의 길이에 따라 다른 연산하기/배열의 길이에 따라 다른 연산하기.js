bd874f08a0d0b67a8c2562f3c5c2348b13dcbc47
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-08 14:37:16 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -0,0 +1,20 @@
+// 정수배열 arr, 숫자 n이 있다.
+// arr의 length가 홀수면 arr의 모든 짝수 인덱스 위치에 n을 더한 배열
+// arr의 length가 짝수면 arr의 모든 홀수 인덱스 위치에 n을 더한 배열
+function solution(arr, n) {
+    // 전개 구문으로 선언
+    const copiedArr = [...arr];
+    
+    let addValue = n;
+    // 반복문
+    for (let i = 0; i < copiedArr.length; i++) {
+    // 1. arr 길이가 홀수면 arr의 모든 짝수인덱스 위치에 n을 더한 배열을 할당한다.
+    if (copiedArr.length % 2 !== 0 && i % 2 === 0) {
+      copiedArr[i] += addValue;
+    // 2. arr 길이가 짝수면 arr의 모든 홀수인덱스 위치에 n을 더한 배열을 할당한다.
+    } else if (copiedArr.length % 2 === 0 && i % 2 !== 0) {
+      copiedArr[i] += addValue;  
+    } 
+  }
+    return copiedArr;
+}
