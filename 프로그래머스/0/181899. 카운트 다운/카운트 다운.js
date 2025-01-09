587336928de015a27cef19cbde363eed9e03e35d
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-09 16:52:17 +0900

    [level 0] Title: 카운트 다운, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -0,0 +1,11 @@
+// start_num, end_num있다.
+// start_num에서 end_num까지 1씩 감소하는 수를 배열로 담아라.
+function solution(start_num, end_num) {
+    const countArrNumbers = [];
+    // 1. for문으로 start_num에서 end_num까지 1씩 감소하기
+    for (let i = start_num; i >= end_num; i--) {
+      countArrNumbers.push(i);
+    }
+    
+    return countArrNumbers;
+}
