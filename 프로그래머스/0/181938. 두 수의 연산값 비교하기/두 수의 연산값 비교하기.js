7afe033e4ee5b1c839c465d7293b198dd62155e3
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-12 22:08:45 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    // 처음에 a 연사 b를 문자열로 바꾼걸 더해서 Number로 바꿔 변수에 할당한다.
+    const sumNumber = Number(String(a) + String(b));
+    // 2 * a * b를 변수로 할당한다.
+    const product = 2 * a * b;
+    // sumNumber가 product보다 크면 sumNumber 아니면 product이다.
+    if (sumNumber > product) {
+      return sumNumber;
+    } else {
+      return product;
+    }
+}
