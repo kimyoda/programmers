4d5fffa8117abab5b2f431ae821f08e23677ea09
kimyoda <rladygks1210@naver.com>
2025-01-12 22:11:13 +0900

    [feat] solution2삼항연사자 활용

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,12 +1,20 @@
 function solution(a, b) {
-    // 처음에 a 연사 b를 문자열로 바꾼걸 더해서 Number로 바꿔 변수에 할당한다.
-    const sumNumber = Number(String(a) + String(b));
-    // 2 * a * b를 변수로 할당한다.
-    const product = 2 * a * b;
-    // sumNumber가 product보다 크면 sumNumber 아니면 product이다.
-    if (sumNumber > product) {
-      return sumNumber;
-    } else {
-      return product;
-    }
+  // 처음에 a 연사 b를 문자열로 바꾼걸 더해서 Number로 바꿔 변수에 할당한다.
+  const sumNumber = Number(String(a) + String(b));
+  // 2 * a * b를 변수로 할당한다.
+  const product = 2 * a * b;
+  // sumNumber가 product보다 크면 sumNumber 아니면 product이다.
+  if (sumNumber > product) {
+    return sumNumber;
+  } else {
+    return product;
+  }
+}
+
+function solution2(a, b) {
+  const sumNumber = Number(string(a) + string(b));
+
+  const product = 2 * a * b;
+
+  return sumNumber > product ? sumNumber : product;
 }
