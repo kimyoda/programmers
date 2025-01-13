a19d1c5e5d5e8d6139fc5e607d85e932c1bb6119
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-13 18:57:56 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -3,9 +3,9 @@
 // 정수 n, m이 있을때 ineq와 eq의 조건에 맞으면 1 아니면 0
 function solution(ineq, eq, n, m) {
     // 템플릿 리터럴을 활용하여 문자열을 하나로 합친 후 변수에 할당.
-    const test = `${ineq}${eq}`;
+    const condition = `${ineq}${eq}`;
     // 조건에 맞는 비교 연산을 수행하고 결과를 반환
-    switch (test) {
+    switch (condition) {
       // 첫번째 조건 ">="에 따라 n이 m보다 크거나 같으면 1 아니면 0
       case ">=":
         return n >= m ? 1 : 0;  
