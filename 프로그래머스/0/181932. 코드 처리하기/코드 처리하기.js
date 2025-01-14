96ebc828bc65095e1588fa0fbb3ce2f1ffc25834
kimyoda <rladygks1210@naver.com>
2025-01-14 20:06:53 +0900

    [fix] 컨벤션 관련 수정

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -7,31 +7,31 @@
 // code[idx]가 "1"이면 mode를 1에서 0으로 바꾼다.
 // 단 mode는 0이고 return하는 ret이 빈 문자열이면 "EMPTY"를 리턴한다.
 function solution(code) {
-    // 문자열 ret과 mode를 초기화하고 할당한다. mode는 0부터 시작한다.
-    let ret = "";
-    let mode = 0;
-    // 반복문을 통해 처음부터 끝까지 순회한다.
-    for (let i = 0; i < code.length; i++) {
+  // 문자열 ret과 mode를 초기화하고 할당한다. mode는 0부터 시작한다.
+  let ret = "";
+  let mode = 0;
+  // 반복문을 통해 처음부터 끝까지 순회한다.
+  for (let i = 0; i < code.length; i++) {
     // 1. mode가 0일 때
-      if (mode === 0) {
-    // 1-2 code[i]가 "1이" 아니고 i가 짝수면 -> ret에 code[i]를 추가한다.
-        if (code[i] !== "1" && i % 2 === 0) {
-          ret += code[i];
-     // 1-3 code[i]가 "1"이면 -> mode를 0에서 1로 바꾼다.
+    if (mode === 0) {
+      // 1-2 code[i]가 "1이" 아니고 i가 짝수면 -> ret에 code[i]를 추가한다.
+      if (code[i] !== "1" && i % 2 === 0) {
+        ret += code[i];
+        // 1-3 code[i]가 "1"이면 -> mode를 0에서 1로 바꾼다.
       } else if (code[i] === "1") {
         mode = 1;
       }
     } else {
-    // 2. mode가 1일때 
-    // 2-1 code[i]가 "1"이 아니고 i가 홀수면 -> ret에 code[i]를 추가한다.
+      // 2. mode가 1일때
+      // 2-1 code[i]가 "1"이 아니고 i가 홀수면 -> ret에 code[i]를 추가한다.
       if (code[i] !== "1" && i % 2 !== 0) {
         ret += code[i];
-    // 2-2 code[i]가 "1"이면 -> mode를 1에서 0으로 바꾼다.    
+        // 2-2 code[i]가 "1"이면 -> mode를 1에서 0으로 바꾼다.
       } else if (code[i] === "1") {
         mode = 0;
       }
     }
   }
-    // 3. ret이 빈 문자열이면 "EMPTY"를 아니면 ret을 리턴한다.
-    return ret === "" ? "EMPTY" : ret;
+  // 3. ret이 빈 문자열이면 "EMPTY"를 아니면 ret을 리턴한다.
+  return ret === "" ? "EMPTY" : ret;
 }
