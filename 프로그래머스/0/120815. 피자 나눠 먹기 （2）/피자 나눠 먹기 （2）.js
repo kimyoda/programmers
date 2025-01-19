96f595c8b283c972e98e3dc90f8cc28e864da94a
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-19 15:31:26 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.05 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -3,12 +3,21 @@
 // n이 주문한 피자를 모두 같은 수의 조각을 먹어야 한다
 function solution(n) {
     // 피자 조각 1부터 순회
-    for (let plates = 1; plates <= n; plates++) {
-          // 6을 곱했을때 plates가 n으로 나눠지는 값 할당  
-      if((6 * plates) % n === 0) {
-        // 조건을 만족하는 plates를 할당한다.
-        return plates;
-      }
-    }
+    // for (let plates = 1; plates <= n; plates++) {
+    //       // 6을 곱했을때 plates가 n으로 나눠지는 값 할당  
+    //   if((6 * plates) % n === 0) {
+    //     // 조건을 만족하는 plates를 할당한다.
+    //     return plates;
+    //   }
+    // }
+      // 피자 1판
+  let price = 6;
+
+  while ((price % n) !== 0) {
+    // 조건을 만족하지 않으면 6을 추가
+    price += 6; 
+  }
+
+  return price / 6; // 최소 피자 판수 반환
     
 }
