44231240e46a6419b20e5dea83942580525ecdff
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-01-24 11:41:01 +0900

    [level 0] Title: 짝수의 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let evenSum = 0;
+    
+    for (let i = 0; i <= n; i++) {
+      if (i % 2 == 0) {
+        evenSum += i;    
+      }
+    }
+    
+    return evenSum;
+}
