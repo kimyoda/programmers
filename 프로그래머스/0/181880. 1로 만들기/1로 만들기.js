a1a1e48ebcc52bb78dd811753b4b7d81a2c66cbd
kimyoda <rladygks1210@naver.com>
2025-01-29 16:50:18 +0900

    [fix] 컨벤션 수정

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -2,24 +2,24 @@
 // 2. 최종값이 1이 되어야 한다.
 // 3. 정수배열 num_list가 있을때 연산의 횟수를 리턴해라.
 function solution(num_list) {
- // 1. 위 1~2에 해당하는 조건에 맞는 연산 구현
- const oneNumberedcounts = num_list.map(el => {
-  // 1로 만드는 연산횟수
-  let count = 0;
-  // 숫자가 1이 될때까지 반복한다.
-  while (el > 1) {
-   if (el % 2 === 0) {
-     // 짝수면 반으로 나누기
-     el = el / 2;
-   } else {
-     // 홀수면 1을 빼고 반으로 나누기
-     el = (el - 1) / 2;
-   }
-   // 연산 횟수가 위 조건에 맞으면 증가한다.
-   count++;
-  }
-  // 최종 연산횟수를 반환한다.
-  return count;  
+  // 1. 위 1~2에 해당하는 조건에 맞는 연산 구현
+  const oneNumberedcounts = num_list.map(el => {
+    // 1로 만드는 연산횟수
+    let count = 0;
+    // 숫자가 1이 될때까지 반복한다.
+    while (el > 1) {
+      if (el % 2 === 0) {
+        // 짝수면 반으로 나누기
+        el = el / 2;
+      } else {
+        // 홀수면 1을 빼고 반으로 나누기
+        el = (el - 1) / 2;
+      }
+      // 연산 횟수가 위 조건에 맞으면 증가한다.
+      count++;
+    }
+      // 최종 연산횟수를 반환한다.
+    return count;  
   });
   // 모든 숫자, 연산 횟수를 합산하여 반환하다.
   return oneNumberedcounts.reduce((a, b) => a + b, 0);
