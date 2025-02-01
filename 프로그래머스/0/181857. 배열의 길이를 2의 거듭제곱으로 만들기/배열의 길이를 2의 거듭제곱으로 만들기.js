710611d3afd10fbb3380e95bca629712aeb5ed70
kimyoda <rladygks1210@naver.com>
2025-02-01 23:04:46 +0900

    [feat] 리팩토링

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.js
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.js
@@ -2,18 +2,20 @@
 // 2. arr의 길이가 2의 정수 거듭제곱이 되도록 arr 뒤에 0을 추가한다.
 // arr의 최소한의 개수로 0을 추가한 배열 리턴
 function solution(arr) {
-  let copiedArr = [...arr];
+  let paddedArr = [...arr];
   // 1. arr 정수의 길이가 2의 거듭제곱인지 판단한다.
   while (true) {
-    // 2. 거듭제곱이 아니면 배열에 0을 추가한다.(Math.log2 라는 매서드를 활용하여 작성함)
+    // 2. 거듭제곱을 표현하는 변수명 할당(Math.log2 라는 매서드를 활용하여 작성함)
+    const logValue = Math.log2(paddedArr.length);
+
     // 3. Number.isInteger() 전발받은 값의 정수 여부를 판별
-    if (Number.isInteger(Math.log2(copiedArr.length))) {
+    // 소수면 false 정수면 true값
+    if (Number.isInteger(logValue)) {
       // 2의 제곱이면 반복 종료
-      break;        
-    } else {
-      // 3. 0을 추가한 배열을 리턴
-      copiedArr.push(0);
+      break;
     }
+    // 0을 추가
+    paddedArr.push(0);
   }
-  return copiedArr;
+  return paddedArr;
 }
