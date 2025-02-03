d26729b9cd15b154de9b371023764e47a2ea2b86
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-03 15:53:11 +0900

    [level 0] Title: 팩토리얼, Time: 2.58 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  // 팩토리얼 결과 저장
+  let result = 1;
+  // 현재 팩토리얼
+  let i = 1;
+  // result가 n을 넘지 않을때 i가 증가하고 result에 i를 곱한 값을 리턴한다.
+  while (result <= n) {
+    i++;
+    result *= i;
+  }
+  // 초과하기 전의 i
+  console.log(i);
+  return i - 1;
+}
