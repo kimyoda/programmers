7ff677fc0dd5659a89cfbf9258ca1bb04dc7bab2
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-06 14:47:42 +0900

    [level 0] Title: 문자열 묶기, Time: 8.92 ms, Memory: 50.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,21 @@
+// strArr 문자열 배열
+// 길이가 같은 문자열끼리 그룹
+// 가장 개수가 많은 그룹의 크기
+function solution(strArr) {
+  // 0. 변수선언
+  let groupCount = {};  
+  let maxCount = 0; 
+  // 1. strArr 문자열의 길이를 map으로 구한다.    
+  const strLengths = strArr.map(el => el.length);
+  // 2. strLengths 문자열 길이에서 순회하여 그룹으로 묶는다
+  strLengths.forEach(el => {
+    // 3. lengthCount객체에 el을 넣고 el이 0이거나 +1일경우를 담는다.    
+    groupCount[el] = (groupCount[el] || 0) + 1;
+    if (groupCount[el] > maxCount) {
+      // 4. 그룹의 최대값
+      maxCount = groupCount[el];
+    }  
+  });
+  // 2. 조건이 같을 때 가장 개수가 많은 그룹의 크기 구하기
+  return maxCount;
+}
