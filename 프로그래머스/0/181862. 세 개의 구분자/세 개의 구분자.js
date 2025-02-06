5fe7a278044f02bd264f4fde3b565e8c60185e22
kimyoda <rladygks1210@naver.com>
2025-02-06 15:16:53 +0900

    [feat] 리팩토링

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -2,7 +2,8 @@
 // 위 3가지로 문자 외에 다른문자가 없으면 빈배열 ["EMPTY"]를 리턴
 function solution(myStr) {
   // 1. split로 a, b, c를 나누는데 join으로 문자열을 할 때 빈칸으로 구분을 유지한다.
- // 2. filter를 통해 문자열인지 아닌지에 따라 새로운 배열로 반환
+  // 2. filter를 통해 문자열인지 아닌지에 따라 새로운 배열로 반환
   const filteredStr = myStr.split("a").join(" ").split("b").join(" ").split("c").join(" ").split(" ").filter(el => el);
+  // 3. filteredStr.length로 비어있으면 ["EMPTY"] 있으면 filteredStr 리턴
   return filteredStr.length ? filteredStr : ["EMPTY"]; 
 }
