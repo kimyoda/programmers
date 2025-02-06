5e02430704ff31a51727d5ca32e0cb5484fb8972
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-06 15:14:46 +0900

    [level 0] Title: 세 개의 구분자, Time: 40.97 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -3,6 +3,6 @@
 function solution(myStr) {
   // 1. split로 a, b, c를 나누는데 join으로 문자열을 할 때 빈칸으로 구분을 유지한다.
  // 2. filter를 통해 문자열인지 아닌지에 따라 새로운 배열로 반환
-  const filteredStr = myStr.split("a").join(" ").split("b").join(" ").split("c").join(" ").split(" ").filter(el => el !== "");
+  const filteredStr = myStr.split("a").join(" ").split("b").join(" ").split("c").join(" ").split(" ").filter(el => el);
   return filteredStr.length ? filteredStr : ["EMPTY"]; 
 }
