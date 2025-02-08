e20d4532740eea9980de01ecd291a51f33003f08
kimyoda <rladygks1210@naver.com>
2025-02-08 17:26:47 +0900

    [fix] 리팩토링

--- 프로그래머스/0/181936. 공배수/공배수.js
+++ 프로그래머스/0/181936. 공배수/공배수.js
@@ -4,3 +4,10 @@ function solution(number, n, m) {
   const isMultiple = number % n === 0 && number % m === 0 ? 1 : 0;
   return isMultiple;
 }
+
+function solution2(number, n, m) {
+  if (number % n === 0 && number % m === 0) {
+    return 1;
+  }
+  return 0;
+}
