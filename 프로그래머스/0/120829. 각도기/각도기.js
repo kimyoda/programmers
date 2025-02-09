34b072e284796cf5aeb7016c707801840d30152d
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-09 22:37:16 +0900

    [level 0] Title: 각도기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,12 +1,12 @@
 function solution(angle) {
   switch(true) {
-    case 0 < angle && angle < 90:
+    case (0 < angle && angle < 90) :
       return 1; // 예각
-    case angle === 90:
+    case (angle === 90) :
       return 2; // 직각
-    case 90 < angle && angle < 180:
+    case (90 < angle && angle < 180) :
       return 3; // 둔각
-    case angle === 180:
+    case (angle === 180) :
       return 4; // 평각
   }
 
