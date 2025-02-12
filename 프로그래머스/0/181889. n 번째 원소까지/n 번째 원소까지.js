633462169a1d9175c26fb602a5421fe25faef742
kimyoda <rladygks1210@naver.com>
2025-02-12 11:52:04 +0900

    [fix] 리팩토링

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,8 +1,14 @@
 function solution(num_list, n) {
   const result = [];
-    
-  for (i = 0; i< n; i++) {
+
+  for (i = 0; i < n; i++) {
     result[i] = num_list[i];
   }
   return result;
+}
+
+function solution2(num_list, n) {
+  const slicedArr = num_list.slice(0, n);
+
+  return slicedArr;
 }
