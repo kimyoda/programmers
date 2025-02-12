822fa90fa1d55b1eab70c76d8266f90d4733a412
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-12 11:48:42 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -2,10 +2,10 @@
 // 2. num_list 첫번째부터 마지막까지 n개 간격으로 저장된 리스트 리턴
 function solution(num_list, n) {
     // num_list를 n개 간격으로 원소를 리스트로 담기
-    const result = num_list.filter((el, idx) => (idx % n === 0));
+    const result = num_list.filter((_, idx) => (idx % n === 0));
     // filter 동작
     // 1. el(값), idx(인덱스) 매개변수를 가지고, 값이 트루일 경우 새로운 배열에 반환되고 false일 시 포함되지 않고, 다음 순회로 넘어간다.
     // 2. 콜백의 조건인 idx가 n이 0일 경우 true로 새로운 배열에 포함, 아닐 시 배열에 포함되지 않고 다음 조건으로 순회한다.
-    // 3. filter의 새로운 배열을 함수의 매개변수에 담고, 이를 선언한 변수에 값을 할당한다.
+    // 3. filter의 새로운 배열을 반환하고, 이를 선언한 변수에 값을 할당한다.
     return result;
 }
