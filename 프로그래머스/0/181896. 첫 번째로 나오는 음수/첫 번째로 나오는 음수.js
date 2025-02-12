3d3c0036fc11d8524da06d576cbe981acc08d006
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-12 11:52:47 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,21 +1,9 @@
 // num_list 정수배열, 첫번째 음수의 인덱스를 return한다
 // 이때 음수가 없으면 -1을 리턴한다.
 function solution(num_list) {
-    // num_list에서 음수가 나오는 인덱스를 리턴
-    // 없으면 -1 리턴
-    const firstNegativeNumber = num_list.findIndex((num) => num < 0);
-
-    return firstNegativeNumber;
+  // num_list에서 음수가 나오는 인덱스를 리턴
+  // 없으면 -1 리턴
+  const firstNegativeNumber = num_list.findIndex((num) => num < 0);
     
-    // let firstNegativeNumber = 0;
-    
-    // for (let i = 0; i < num_list.length; i++) {
-    //   if (num_list[i] < 0) {
-    //     firstNegativeNumber = i;
-    //     break;
-    //   } else {
-    //     firstNegativeNumber = -1;
-    //   }
-    // }
-    // return firstNegativeNumber;
+  return firstNegativeNumber;
 }
