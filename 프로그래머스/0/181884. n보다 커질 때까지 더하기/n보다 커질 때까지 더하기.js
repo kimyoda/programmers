ce0d13271ac44bdfcc69ae381eb090946e3af2e6
kimyoda <rladygks1210@naver.com>
2025-02-12 11:43:50 +0900

    [fix] 리팩토링

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -3,12 +3,24 @@ function solution(numbers, n) {
   let sumsElement = 0; // 누적 합을 저장할 변수 초기화
 
   for (let i = 0; i < numbersLength; i++) {
-    sumsElement += numbers[i]; 
+    sumsElement += numbers[i];
 
     if (sumsElement > n) {
-      return sumsElement; 
+      return sumsElement;
     }
   }
 
-  return sumsElement; 
+  return sumsElement;
+}
+
+function solution2(number, n) {
+  let sumsElement = 0;
+
+  numbers.forEach((el) => {
+    if (sumsElement > n) {
+      return;
+    }
+    sumsElement += el;
+  });
+  return sumsElement;
 }
