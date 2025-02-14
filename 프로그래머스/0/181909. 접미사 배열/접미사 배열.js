4a1f3173cd3c293d289e971453804e5893c87b3f
kimyoda <rladygks1210@naver.com>
2025-02-14 14:44:45 +0900

    [feat] 리팩토링

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -2,15 +2,15 @@
 // 모든 접미사를 사전순으로 정렬한
 // 문자열 배열을 return한다
 function solution(my_string) {
-    const suffixList = [];
-    // 스프레드 연산자로 얕은복사
-    const copiedSuffixList = [...suffixList];
-    // 문자열 길이만큼 반복 후 문자열의 현재 인덱스부터 끝까지 부분 문자열 추출하여 문자열에 추가, 그 값을 push메서드로 배열에 추가한다. 
-    for (let i = 0; i < my_string.length; i++) {
-      copiedSuffixList.push(my_string.slice(i))
-    }
-    // 값이 담긴 배열자체를 sort로 정렬시켜 담는다.
-    copiedSuffixList.sort();
+  const suffixList = [];
+  // 스프레드 연산자로 얕은복사
+  const copiedSuffixList = [...suffixList];
+  // 문자열 길이만큼 반복 후 문자열의 현재 인덱스부터 끝까지 부분 문자열 추출하여 문자열에 추가, 그 값을 push메서드로 배열에 추가한다. 
+  for (let i = 0; i < my_string.length; i++) {
+    copiedSuffixList.push(my_string.slice(i))
+  }
+  // 값이 담긴 배열자체를 sort로 정렬시켜 담는다.
+  copiedSuffixList.sort();
     
-    return copiedSuffixList;
+  return copiedSuffixList;
 }
