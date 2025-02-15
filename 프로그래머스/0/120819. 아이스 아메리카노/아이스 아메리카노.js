f5ba7073468d1a359b58589a2837057e465784c4
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-15 20:22:46 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,16 +1,8 @@
 function solution(money) {
-  const restoftheCount = [];
+    const restoftheCount = [];
 
-  restoftheCount.push(parseInt(money / 5500));
-  restoftheCount.push(money % 5500);
+    restoftheCount.push(parseInt(money / 5500));
+    restoftheCount.push(money % 5500);
 
-  return restoftheCount;
-}
-
-function solution2(money) {
-  const CupofAmericanoPrice = 5500;
-  const maxCupCount = Math.floor(money / CupofAmericanoPrice);
-  const restoftheCount = money - maxCupCount * CupofAmericanoPrice;
-
-  return [maxCupCount, restoftheCount];
+    return restoftheCount;
 }
