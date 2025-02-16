bde64d415d47c024ec41378dcca83e7ffb2a8a68
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-16 23:48:59 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,22 @@
+// 1. 10만원 이상 5%
+// 2. 30만원 이상 10%
+// 3. 50만원 이상 20%
+function solution(price) {
+  // 1. if문으로 각 조건에 맞게 비교를 한다. 변수 할당
+  let discount = 0;
+  // 2. 각 조건에 맞게 if문을 거친다.
+  // 500,000원을 넘길때 20%
+  if (price >= 500000) {
+    discount = 0.2;
+    // 300,000원을 넘길때 10%
+  } else if (price >= 300000) {
+    discount = 0.1;
+    // 100,000원을 넘길때 5%
+  } else if (price >= 100000) {
+    discount = 0.05;
+  }
+  // 소수점 이하를 버린 정수를 리턴, price의 퍼센트에 곱해서 리턴한다.
+  return parseInt(price * (1 - discount));
+}
+
+    
