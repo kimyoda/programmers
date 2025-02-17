8b22f9460a4b352e26e56747736b0834d2011c25
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-17 20:48:17 +0900

    [level 0] Title: 개미 군단, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,17 @@
+// 개미 군단이 사냥감의 체력에 맞는 병력을 데리고 나가려고 한다.
+// 장군개미 5, 병정개미 3, 일개미 1
+// 23여치 장군개미 4마리 병정 1마리로도 가능
+// hp는 자연수
+function solution(hp) {
+  // 1. 담을 변수
+  let antCount = 0;
+  // 2. 장군 개미 공격력만큼 깍인다.
+  antCount += Math.floor(hp / 5);
+  hp %= 5;
+  // 3. 병정 개미 공격력만큼 깍인다
+  antCount += Math.floor(hp / 3);
+  hp %= 3;
+  // 4. 일개미 공격력 만큼 깍인다.
+  antCount += hp;
+  return antCount;
+}
