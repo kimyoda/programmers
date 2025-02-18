130e027d40b4d2790120f81f4f3d28020687d331
kimyoda <rladygks1210@naver.com>
2025-02-18 23:40:22 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -2,8 +2,20 @@
 // height보다 큰 사람 수를 구해라
 function solution(array, height) {
   // filter를 통해 array 요소 중 height보다 큰 요소들을 배열로 필터링한 후, 해당 요소의 개수를 구한다.
-  const tallerStudents = array.filter(el => el > height).length;
-    
+  const tallerStudents = array.filter((el) => el > height).length;
+
   return tallerStudents;
 }
 
+function solution2(array, height) {
+  // 변수 선언
+  let tallerStudents = 0;
+
+  array.forEach((el) => {
+    if (el > height) {
+      tallerStudents++;
+    }
+  });
+
+  return tallerStudents;
+}
