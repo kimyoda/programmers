91daf23e4e4749a77d101d3d743cdd1f7a497f07
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-18 23:29:07 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,6 +1,10 @@
+// num_list 정수 배열
+// num_list 원소 중 짝수와 홀수 개수 담은 배열을 리턴
 function solution(num_list) {
-  const evenNumbers = num_list.filter(num => num % 2 === 0).length;
-  const oddNumbers = num_list.filter(num => num % 2 === 1).length;
+  // 1. 짝수만 모아놓은 배열, filter로 짝수만 걸러서 새로운 배열의 개수로로 할당한다.
+  const evenCount = num_list.filter(el => el % 2 === 0).length;
+  // 2. 홀수만 모아놓은 배열, filter로 홀수만 걸러서 새로운 배열개수로 할당한다.
+  const oddCount = num_list.filter(el => el % 2 !== 0).length;
   
-  return [evenNumbers, oddNumbers];
+  return [evenCount, oddCount];
 }
