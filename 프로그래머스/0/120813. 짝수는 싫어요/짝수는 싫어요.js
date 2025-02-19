f8485470ae7bf089b8ef0475f5d5a2b8d6701075
kimyoda <rladygks1210@naver.com>
2025-02-19 09:59:21 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,10 +1,22 @@
 // n이 매개변수, 이하의 홀수가 오름차순으로 담긴 배열을 리턴
 function solution(n) {
   // 1. 1부터 n까지의 숫자를 배열로 생성한다.
-  const copiedOddNumbers = Array.from({length: n}, (_, i) => i + 1);
+  const copiedOddNumbers = Array.from({ length: n }, (_, i) => i + 1);
 
   // 2. filter로 n 이하의 홀수만 배열로 필터링하여 할당한다.
-  const filteredOddNumbers = copiedOddNumbers.filter(el => el % 2 !== 0);
+  const filteredOddNumbers = copiedOddNumbers.filter((el) => el % 2 !== 0);
 
   return filteredOddNumbers;
+}
+
+function solution2(n) {
+  let oddNumbers = 0;
+  // 1. 1부터 n까지의 숫자를 순회한다.
+  for (let i = 1; i < n; i++) {
+    // 2. if문을 통해 짝수가 아닐때 push로 배열에 추가한다.
+    if (i % 2 !== 0) {
+      oddNumbers.push(i);
+    }
+  }
+  return oddNumbers;
 }
