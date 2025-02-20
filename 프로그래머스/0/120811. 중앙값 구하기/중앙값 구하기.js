67c3be2d6923b3b8c06ffd9a09cf8c2781f071e3
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-20 14:27:16 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,13 +1,11 @@
 // 중앙값은 크기의 순서대로 정렬했을 때 중앙에 위치하는 값
 // 정수 array 가 매개변수일때 중앙값을 리턴해라.
 function solution(array) {
-  const arrayLength = array.length;
-  const middleIndex = Math.floor(arrayLength / 2); 
+  // 1. spread연산자를 활용하여 오름차순으로 정렬.
+  const sortedArr = [...array].sort((a, b) => a - b);
+  // 2. 정렬된 배열에서 중앙 값의 인덱스를 구하고, 요소를 가져온다.
+  const middleValue = sortedArr[Math.floor(sortedArr.length / 2)];
 
-  array.sort((a, b) => a - b);
-
-  const middleElement = array[middleIndex];
-
-  return middleElement;
+  return middleValue;
 }
 
