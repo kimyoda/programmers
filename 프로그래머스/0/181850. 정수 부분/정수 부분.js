b530ddb015ea2f964d12298beeb671b4eb554a76
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-22 09:31:52 +0900

    [level 0] Title: 정수 부분, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181850. 정수 부분/정수 부분.js
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.js
@@ -1,5 +1,6 @@
 function solution(flo) {
-  const VARIABLE_CONVERSION_VALUES = parseInt(flo);
+  // parseInt 혹은 Math.floor 등으로 정수로 할당하기, 스네이크 케이스 활용
+  const VARIABLE_CONVERSION_VALUES = Math.floor(flo);
   
   return VARIABLE_CONVERSION_VALUES;
 }
