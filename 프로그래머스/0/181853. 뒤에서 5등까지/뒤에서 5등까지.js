e4e1e7be30a55d087462e2913cb2d7c0914b529a
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-22 09:28:07 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -1,17 +1,11 @@
 // 정수배열 num_list가 있다.
 // num_list에 가장 작은 5개의 수를 오름차순으로 담은걸 리턴해라.
 function solution(num_list) {
-    // 1. num_list의 복사본 배열을 할당하기
-    const copiedMinNumberList = [...num_list]
-    // 2. num_list의 가장 작은 5개 수 구하기 오름차순으로 정렬한다.(sort) 
-    const smallestFiveNumber = copiedMinNumberList.sort((a, b) => a - b);
-    // 2. slice로 sort 정렬된 배열을 0번부터 5번까지 리턴하여 종료한다.
-    return smallestFiveNumber.slice(0, 5);
-}
+  // 1. num_list의 가장 작은 개수대로 정렬하기
+  const copiedNumList = [...num_list];
 
-function solution2(num_list) {
-    // solution 2
-    const smallestFiveNumberList = [...num_list].sort((a, b) => a - b).slice(0, 5);
+  // 2. sort로 정렬 한뒤 slice로 5개가 리턴하도록하기
+  const sortedNumbers = copiedNumList.sort((a, b) => a - b).slice(0, 5);
     
-    return smallestFiveNumberList;
+  return sortedNumbers;
 }
