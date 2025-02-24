a07750eba3b8487e52fa0838db9d45d5488c0128
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-24 10:06:35 +0900

    [level 0] Title: rny_string, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.js
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -1,6 +1,6 @@
 // 1. rny_string을 모든 m을 rn으로 바꾼 문자열 리턴(영소문자)
 function solution(rny_string) {
-    const changeChar = rny_string.replaceAll("m", "rn");
+    const changedString = rny_string.replaceAll("m", "rn");
     
-    return changeChar;
+    return changedString;
 }
