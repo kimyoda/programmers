b3d84065c64324683d688059bf555c0ab68dddae
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-24 10:01:00 +0900

    [level 0] Title: 약수 구하기, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/README.md
+++ 프로그래머스/0/120897. 약수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 약수 구하기 - 120897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120897?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120897) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.28 ms
+메모리: 33.5 MB, 시간: 0.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 28일 21:57:54
+2025년 02월 24일 10:00:58
 
 ### 문제 설명
 
--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -2,8 +2,6 @@
 // 2. n의 약수의 오름차순을 구한다
 // 3. 오름차순을 담은 배열을 return 한다.
 function solution(n) {
-  const answer = [];
-  // 약수를 나눌 변수 설정
   const divisorArray = [];
   // 반복문을 통하여 n만큼 반복
   for (let i = 1; i <= n; i++) {
@@ -12,10 +10,6 @@ function solution(n) {
       divisorArray.push(i);       
     }
   }
-  // sort를 활용하여 오름차순식으로 divsisorArray에 담기   
-  divisorArray.sort((a, b) => a - b);
-  // 쓰레드연산자와 push메서드를 활용하여 answer에 담기
-  answer.push(...divisorArray)
-    
-  return answer;
+
+  return divisorArray;
 }
