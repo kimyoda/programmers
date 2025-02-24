148ee9758c5b8319c64e44d62f0091384dec777f
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-25 08:12:06 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.05 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -1,11 +1,8 @@
 // my_string 영소문자 문자열, alp 영소 문자 1글자
 // my_string에서 alp에 해당하는 모든 글자를 대문자로 바꾸기
 function solution(my_string, alp) {
-  const changedChar = my_string.replaceAll(alp, alp.toUpperCase());
-
-  return changedChar;
-}
-
-function solution2(my_string, alp) {
+//   const changedChar = my_string.replaceAll(alp, alp.toUpperCase());
+    
+//   return changedChar;
   return my_string.split(alp).join(alp.toUpperCase());
 }
