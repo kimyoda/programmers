42b96f70125b7034bf5fc13a8d9199007799a101
kimyoda <rladygks1210@naver.com>
2025-02-25 08:15:07 +0900

    [fix] 리팩토링 및 soultion2 추가

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -1,8 +1,15 @@
 // my_string 영소문자 문자열, alp 영소 문자 1글자
 // my_string에서 alp에 해당하는 모든 글자를 대문자로 바꾸기
 function solution(my_string, alp) {
-//   const changedChar = my_string.replaceAll(alp, alp.toUpperCase());
-    
-//   return changedChar;
-  return my_string.split(alp).join(alp.toUpperCase());
+  // 1. replaceAll을 활용하여 alp의 해당하는 부분을 대문자로 바꾼다.
+  const changedChar = my_string.replaceAll(alp, alp.toUpperCase());
+
+  return changedChar;
+}
+
+function solution2(my_string, alp) {
+  // 1. split와 join을 활용하여 alp를 대문자로 바꾸고 문자열로 변환하여 할당한다.
+  const changedChar = my_string.split(alp).join(alp.toUpperCase());
+
+  return changedChar;
 }
