dda02630442e2ba8e37c94a1297095b7b4e74bfc
kimyoda <rladygks1210@naver.com>
2025-02-24 10:09:24 +0900

    [fix] 리팩토링 및 soltuion2 추가

--- 프로그래머스/0/181863. rny＿string/rny＿string.js
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -1,6 +1,13 @@
 // 1. rny_string을 모든 m을 rn으로 바꾼 문자열 리턴(영소문자)
 function solution(rny_string) {
-    const changeChar = rny_string.replaceAll("m", "rn");
-    
-    return changeChar;
+  // replaceAll로 m을 rn으로 전부 바꾼다.
+  const changedChar = rny_string.replaceAll("m", "rn");
+
+  return changedChar;
+}
+
+function solution2(rny_string) {
+  const changedChar = rny_string.split("m").join("rn");
+
+  return changedChar;
 }
