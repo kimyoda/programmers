a8157a9970f4aec55a898d19da7948fa8585f3fa
kimyoda <rladygks1210@naver.com>
2025-02-26 09:31:37 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -2,10 +2,13 @@
 // myString의 부분 문자열 중에 pat이 존재하면 1 아니면 0을 반환
 function solution(myString, pat) {
   const checkedStr = myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
-  
+
   return checkedStr;
 }
 
+function solution2(myString, pat) {
+  const checkedStr =
+    myString.toUpperCase().indexOf(pat.toUpperCase()) !== -1 ? 1 : 0;
 
-
-
+  return checkedStr;
+}
