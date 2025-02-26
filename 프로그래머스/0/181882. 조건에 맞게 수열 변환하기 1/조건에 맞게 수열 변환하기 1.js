60abfa1e3575ffdcb2a693ae0f7580cfa403bdd9
kimyoda <rladygks1210@naver.com>
2025-02-26 09:48:59 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -4,11 +4,11 @@
 function solution(arr) {
   const transFormedSequence = [];
   // forEach를 활용하여 배열을 순회한다.
-  arr.forEach(el => {
+  arr.forEach((el) => {
     // 1. arr의 el이 50보다 크거나 같은 짝수
     if (el >= 50 && el % 2 === 0) {
       // el을 2로 나눈값을 배열에 추가한다.
-      transFormedSequence.push(el / 2); 
+      transFormedSequence.push(el / 2);
       // 2. arr의 el이 50보다 작은 홀수
     } else if (el < 50 && el % 2 !== 0) {
       // 3. el을 2로 곱한다.
@@ -19,4 +19,16 @@ function solution(arr) {
     }
   });
   return transFormedSequence;
+}
+
+function solution2(arr) {
+  const transformedValue = arr.map((value) => {
+    if (value >= 50 && value % 2 === 0) {
+      return value / 2; // 50 이상 짝수 일때 나누기
+    } else if (value < 50 && value % 2 !== 0) {
+      return value * 2; // 50 이하 홀수 일때 곱하기
+    }
+    return value; // 그 외의 값
+  });
+  return transformedValue; // 최종 변환값 반환
 }
