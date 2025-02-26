c0182003977e5785cbd6b77ac09d27e73304d32c
kimyoda <rladygks1210@naver.com>
2025-02-26 09:39:38 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -6,9 +6,30 @@ function solution(num_list) {
   const isSum = numListLength >= 11;
   // 2. 초기값 설정
   const initalValue = isSum ? 0 : 1;
-  
+
   // 3. reduce 메서드를 통해 isSum이면 합 아니면 곱을 초기값을 통해 구한다.
-  const arrSumOrProduct = num_list.reduce((acc, cur) => isSum ? acc + cur : acc * cur, initalValue);
-    
+  const arrSumOrProduct = num_list.reduce(
+    (acc, cur) => (isSum ? acc + cur : acc * cur),
+    initalValue
+  );
+
+  return arrSumOrProduct;
+}
+
+function solution2(num_list) {
+  // 초기값 설정
+  let arrSumOrProduct = num_list.length >= 11 ? 0 : 1;
+  // 11개 이상이면 합
+  const isSum = num_list.length >= 11;
+
+  // forEach문 사용
+  num_list.forEach((el) => {
+    // if문을 통해 isSum일때 더한값을 하고 아닐 때 곱한다.
+    if (isSum) {
+      arrSumOrProduct += el;
+    } else {
+      arrSumOrProduct *= el;
+    }
+  });
   return arrSumOrProduct;
 }
