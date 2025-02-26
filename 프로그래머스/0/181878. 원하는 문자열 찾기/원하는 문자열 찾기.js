5438fd9420c22c0d335a210b2057762cf69ccffe
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-26 09:29:04 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.14 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,9 +1,9 @@
 // 문자열 myString, pat이있다.
 // myString의 부분 문자열 중에 pat이 존재하면 1 아니면 0을 반환
 function solution(myString, pat) {
-  const checkedChar = myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
+  const checkedStr = myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
   
-  return checkedChar;
+  return checkedStr;
 }
 
 
