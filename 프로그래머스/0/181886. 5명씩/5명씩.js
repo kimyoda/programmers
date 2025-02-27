834095e78da740d74994de7cd2c06c08be715b18
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-27 10:50:38 +0900

    [level 0] Title: 5명씩, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,6 +1,7 @@
 // name가 5명씩 탑승가능한 놀이기구를 타기위해 줄을 서 있는 문자열 리스트
 // 앞에서부터 5명씩 묶은 그룹을 포함한다. 가장 앞에 있는 사람의 이름을 포함한다.
 function solution(names) {
+  // names에서 5명씩 묶은 값을 새로운 배열로 반환한다.
   const groups = names.filter((_, idx) => (idx % 5 === 0));
 
   return groups;
