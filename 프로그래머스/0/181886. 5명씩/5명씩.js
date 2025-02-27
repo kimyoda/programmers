dd01cd30a606ebbb70ddbb88514aedb2d21625d9
kimyoda <rladygks1210@naver.com>
2025-02-27 10:52:42 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -2,7 +2,19 @@
 // 앞에서부터 5명씩 묶은 그룹을 포함한다. 가장 앞에 있는 사람의 이름을 포함한다.
 function solution(names) {
   // names에서 5명씩 묶은 값을 새로운 배열로 반환한다.
-  const groups = names.filter((_, idx) => (idx % 5 === 0));
+  const groups = names.filter((_, idx) => idx % 5 === 0);
+
+  return groups;
+}
+
+function solution2(names) {
+  // 값을 할당할 변수
+  let groups = [];
+
+  // for문을 통해 5명씩 순회하여 추가하기
+  for (let i = 0; i < names.length; i += 5) {
+    groups.push(names[i]);
+  }
 
   return groups;
 }
