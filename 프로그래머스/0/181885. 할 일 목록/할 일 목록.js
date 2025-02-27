77463a6e22d0ff9723b581a024796b44683f2691
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-02-27 10:48:37 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,17 +1,7 @@
+// 문자열 배열 todo_list, bolean 배열 finished
+// todo_list에서 마치지 못한 일들을 순선대로 담은 문자열 배열 리턴
 function solution(todo_list, finished) {
-  const unfinishedWorks = [];
-  const todo_listLength = todo_list.length;
-
-  for (let i = 0; i < todo_listLength; i++) {
-    if (!finished[i]) {
-      unfinishedWorks[unfinishedWorks.length] = todo_list[i];
-    }
-  }
-  return unfinishedWorks;
-}
-
-function solution2(todo_list, finished) {
-  const unfinishedWork = todo_list.filter((_, idx) => !finished[idx]);
-
-  return unfinishedWork;
+  // filter를 통해 todo_list에서 끝나지않은 finished의 값을 false인 부분만 필터링하여 새로운 배열로 담는다. 
+  const filteredTodoList = todo_list.filter((_, idx) => !finished[idx]);
+  return filteredTodoList;
 }
