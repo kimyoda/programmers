2e167fe3ab3ca43393ad05feac179977d6354844
kimyoda <rladygks1210@naver.com>
2025-02-28 14:53:32 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -3,7 +3,7 @@ function solution(num_list) {
   let sumOddNumbers = 0;
   let sumEvenNumbers = 0;
   let sumNumbers = 0;
-  // 2. forEach를 통해 el이 홀수일때, 짝수일 때 값을 담기  
+  // 2. forEach를 통해 el이 홀수일때, 짝수일 때 값을 담기
   num_list.forEach((el, idx) => {
     // 짝수번째 원소들의 합
     if (idx % 2 === 0) {
@@ -17,4 +17,18 @@ function solution(num_list) {
   sumNumbers = Math.max(sumEvenNumbers, sumOddNumbers);
 
   return sumNumbers;
+}
+
+function solution2(num_list) {
+  // 1. 짝수번째 원소들의 합을 filter와, reduce로 구하기
+  // 2. filter로 짝수번째 원소값만 남긴 뒤 합을 구하기
+  const sumEvenNumbers = num_list
+    .filter((_, idx) => idx % 2 === 0)
+    .reduce((acc, cur) => acc + cur, 0);
+  // 3. 홀수번째 원소들의 원소값만 남기고 합을 구하기
+  const sumOddNumbers = num_list
+    .filter((_, idx) => idx % 2 !== 0)
+    .reduce((acc, cur) => acc + cur, 0);
+
+  return Math.max(sumEvenNumbers, sumOddNumbers);
 }
