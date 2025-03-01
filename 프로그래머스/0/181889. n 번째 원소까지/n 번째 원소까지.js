05c31008de4bfd4b29813e51fbeab4b1b3f315d0
kimyoda <rladygks1210@naver.com>
2025-03-01 10:15:39 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -3,6 +3,22 @@
 function solution(num_list, n) {
   // slice를 통해 첫 번째 원소부터 n까지 잘라서 할당하기
   const slicedArr = num_list.slice(0, n);
-    
+
+  return slicedArr;
+}
+
+function solution2(num_list, n) {
+  // 값을 할당할 변수.
+  let slicedArr = [];
+
+  num_list.some((el, idx) => {
+    // idx가 n보다 크기전까지 반복한다
+    if (idx < n) {
+      slicedArr.push(el);
+      return false;
+    }
+    // n개를 채우면 종료한다.
+    return true;
+  });
   return slicedArr;
 }
