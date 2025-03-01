3d6a3f7bd65e46a31ea5df5725cc952b699d526f
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-03-01 10:11:02 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,14 +1,8 @@
+// 정수배열 num_list, 정수 n
+// 첫번째 원소부터 n번째 원소까지 값 구하기
 function solution(num_list, n) {
-  const result = [];
-
-  for (i = 0; i < n; i++) {
-    result[i] = num_list[i];
-  }
-  return result;
-}
-
-function solution2(num_list, n) {
+  // slice를 통해 첫 번째 원소부터 n까지 잘라서 할당하기
   const slicedArr = num_list.slice(0, n);
-
+    
   return slicedArr;
 }
