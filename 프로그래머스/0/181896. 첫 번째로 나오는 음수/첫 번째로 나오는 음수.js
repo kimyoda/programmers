89add8c1ac0b6f17985e16906787d54b770599e5
kimyoda <rladygks1210@naver.com>
2025-03-01 10:43:14 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -3,16 +3,21 @@
 function solution(num_list) {
   // num_list에서 음수가 나오는 인덱스를 리턴
   // 없으면 -1 리턴
-//   const firstNegativeNumber = num_list.findIndex((num) => num < 0);
-    
-//   return firstNegativeNumber;
-  let firstNegativeNumber = -1;
+  const firstNegativeNumber = num_list.findIndex((el) => el < 0);
 
+  return firstNegativeNumber;
+}
+
+function solution2(num_list) {
+  // 초기 값을 음수로 설정한다.(음수가 없을 경우)
+  let firstNegativeNumber = -1;
+  // 음수를 찾으면 인덱스를 저장한다.
   for (let i = 0; i < num_list.length; i++) {
     if (num_list[i] < 0) {
       firstNegativeNumber = i;
+      // 첫 번째 음수를 찾으면 종료한다.
       break;
-    } 
+    }
   }
   return firstNegativeNumber;
 }
