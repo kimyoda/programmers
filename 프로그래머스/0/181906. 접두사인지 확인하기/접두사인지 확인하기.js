f87fa09ee557002cac75eb71a8e728d7659c050b
kimyoda <rladygks1210@naver.com>
2025-03-02 16:08:34 +0900

    [fix] 리팩토링 및 solution2

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -2,17 +2,18 @@
 // 2. my_string의 접두사 is_prefix면
 // 3. true일때 1 아니면 false로 리턴한다.
 function solution(my_string, is_prefix) {
-    // 1. my_string의 is_prefix의 값이 true인지 false인 지 구하고 그 true면 1 아니면 0을 담아 변수 선언에 값을 할당한다.
-    // 2. startsWith라는 메서드를 통해 문자열의 문자로 시작하는 확인 후 결과를 true 혹은 false로 반환한다.
-    // 3. +단항연산자를 활용해 1과 0을 리턴한다.
-    const hasPrefix = my_string.startsWith(is_prefix);
-    return +hasPrefix;
-    
-    // 2번째 풀이(slice 활용)
-    // 0번 인덱스부터 is_prefix의 길이만큼 추출한 값이 
-    // is_prefix값과 맞을때 1의 값, 아닐 때 0을 값을 반환한다.
-//     const prefixString = my_string.slice(0, is_prefix.length) === is_prefix ? 1 : 0;
-    
-//     return prefixString;
-    
+  // 1. my_string의 is_prefix의 값이 true인지 false인 지 구하고 그 true면 1 아니면 0을 담아 변수 선언에 값을 할당한다.
+  // 2. startsWith라는 메서드를 통해 문자열의 문자로 시작하는 확인 후 결과를 true 혹은 false로 반환한다.
+  // 3. +단항연산자를 활용해 1과 0을 리턴한다.
+  const hasPrefix = my_string.startsWith(is_prefix);
+
+  return +hasPrefix;
+}
+
+function solution2(my_string, is_prefix) {
+  // 1. slice를 활용하여 0번 인덱스부터 is_prefix의 길이만큼 추출 하여 is_prefix 1이거나 0일때 값을 할당한다.
+  const prefixString =
+    my_string.slice(0, is_prefix.length) === is_prefix ? 1 : 0;
+
+  return prefixString;
 }
