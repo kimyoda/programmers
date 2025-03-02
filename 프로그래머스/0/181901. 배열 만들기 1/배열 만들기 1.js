c41c1f1fc2e9c01fda8257d0daa0eafde43a1f02
kimyoda <rladygks1210@naver.com>
2025-03-02 10:10:06 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -2,12 +2,21 @@
 // 1 이상 n 이하의 정수중 k의 배수를
 // 오름차순으로 저장한 배열을 리턴.
 function solution(n, k) {
-    // 1. 변수 선언
-    const answer = [];
-    // 2. result 길이구하는걸 고민하자
-    // n이있고 k가 반복해서 n만큼 안넘고
-    for (let i = 1; i <= n / k; i++) {
-    answer.push(i * k);   
-    }
-    return answer;
+  // 1. 변수 선언
+  const answer = [];
+  // 2. result 길이구하는걸 고민하자
+  // n이있고 k가 반복해서 n만큼 안넘고
+  for (let i = 1; i <= n / k; i++) {
+    answer.push(i * k);
+  }
+  return answer;
+}
+
+function solution2(n, k) {
+  // Array.from을 활용하여 n이 k의 오름차순으로 오는 배열을 만들어 반환한다.(filter로 값을 필터하여 담는다).
+  const filteredArr = Array.from({ length: n }, (_, i) => i + 1).filter(
+    (el) => el % k === 0
+  );
+
+  return filteredArr;
 }
