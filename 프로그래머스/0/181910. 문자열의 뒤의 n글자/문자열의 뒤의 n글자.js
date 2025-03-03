b434b055e7f67b0d956ca9605aa6c49320a62b80
kimyoda <rladygks1210@naver.com>
2025-03-03 13:22:22 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -3,6 +3,13 @@
 function solution(my_string, n) {
   // 문자열 my_string의 뒤쪽 n글자를 추출하기 위해 slice 사용. -n을 통해 문자열의 끝에서 n글자를 가져온다.
   const lastStr = my_string.slice(-n);
-  
+
+  return lastStr;
+}
+
+function solution2(my_string, n) {
+  // substring을 활용해여 n글자를 가져온다
+  const lastStr = my_string.substring(my_string.length - n);
+
   return lastStr;
 }
