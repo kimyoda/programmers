92dc8458bab659906b1657f7f7a6129cbb503712
kimyoda <141495978+kimyoda@users.noreply.github.com>
2025-03-04 10:18:05 +0900

    [level 0] Title: 카운트 업, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -3,19 +3,8 @@
 function solution(start_num, end_num) {
   const countNumbers = [];
   // i가 0부터 반복해서, 정수 start_num에서 end_num을 넘지않고 반복하고 start_num을 i만큼 반복한 값을 새로운 배열로 반환한다.
-  for (let i = 0; i <= (end_num - start_num); i++) {
+  for (let i = 0; i <= end_num - start_num; i++) {
     countNumbers.push(start_num + i);
   }
   return countNumbers;
-}
-
-function solution2(start_num, end_num) {
-  // 변수 선언
-  const countNumberArr = [];
-
-  // Array.form으로 차례로 담은 리스트 리턴
-  countNumberArr.push(...Array.from({ length: end_num - start_num + 1}, (_, i) => start_num + i));
-
-  return countNumberArr;
-  
 }
