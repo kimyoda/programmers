d0815a2f158c0fb84e26034c2f7bfc15b0cd7ed1
kimyoda <rladygks1210@naver.com>
2025-03-12 20:01:25 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,11 +1,17 @@
 // 정수로 이루어진 문자열 n_str이 있다.
 // n_str의 가장 왼쪽에 청므 등장하는 0을 뗀 문자열 리턴(0만 뗀다)
 function solution(n_str) {
-    // n_str이 있을때 가장 왼쪽에 있는 0만 제거하고 값을 변수에 할당해 리턴한다.
-    
-   // 1. n_str을 Number로 숫자로 만든다.
-   // 2. 정수면 저 앞에 0 다 없어진다.
-   // 3. 정수화된 숫자를 문자열로 변환한다.
-    const convertedNumber = Number(n_str);
-    return convertedNumber.toString();
+  // n_str이 있을때 가장 왼쪽에 있는 0만 제거하고 값을 변수에 할당해 리턴한다.
+
+  // 1. n_str을 Number로 숫자로 만든다.
+  // 2. 정수면 저 앞에 0 다 없어진다.
+  // 3. 정수화된 숫자를 문자열로 변환한다.
+  const convertedNumber = Number(n_str);
+  return convertedNumber.toString();
+}
+
+function solution2(n_str) {
+  const convertedNumber = parseInt(n_str);
+
+  return convertedNumber.toString();
 }
