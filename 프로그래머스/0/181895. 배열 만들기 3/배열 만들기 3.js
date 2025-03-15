4c69f6cf478689b75e48a3c5a3fc4d85369a0278
kimyoda <rladygks1210@naver.com>
2025-03-15 14:30:10 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -3,8 +3,21 @@
 function solution(arr, intervals) {
   // 0. 변수선언을 구조 분해 할당을 통해 intervals를 각 구간 변수로 분리한다.
   const [[a1, b1], [a2, b2]] = intervals;
-  // 1. arr배열에서 intervals의 인덱스 값을 조건에 맞게 자르고 스프레드 연산자를 활용하여 병합한다. 
-  const mergedIntervals = [...arr.slice(a1, b1 + 1), ...arr.slice(a2,  b2 + 1)];
+  // 1. arr배열에서 intervals의 인덱스 값을 조건에 맞게 자르고 스프레드 연산자를 활용하여 병합한다.
+  const mergedIntervals = [...arr.slice(a1, b1 + 1), ...arr.slice(a2, b2 + 1)];
   // 3. 값을 리턴한다.
   return mergedIntervals;
+}
+
+function solution2(arr, intervals) {
+  // 변수를 담을 값
+  let result = [];
+
+  // forEach문 활용하여
+  intervals.forEach(([start, end]) => {
+    // push로 하여 담기
+    result.push(...arr.slice(start, end + 1));
+  });
+
+  return result;
 }
