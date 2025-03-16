14d75f61a1c4ce48091b36b60f364f778b41b6b9
kimyoda <rladygks1210@naver.com>
2025-03-16 23:54:34 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.js
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.js
@@ -1,5 +1,11 @@
 function solution(n) {
   const numberChangeString = n.toString();
- 
+
+  return numberChangeString;
+}
+
+function solution2(n) {
+  const numberChangeString = String(n);
+
   return numberChangeString;
 }
