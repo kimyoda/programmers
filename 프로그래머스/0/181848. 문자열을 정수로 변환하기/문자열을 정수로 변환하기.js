7738b8057f1d49fd80d06d7401b540fffe55b64b
kimyoda <rladygks1210@naver.com>
2025-03-17 10:04:05 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.js
+++ 프로그래머스/0/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.js
@@ -1,5 +1,11 @@
 function solution(n_str) {
   const changedNumber = Number(n_str);
-    
+
+  return changedNumber;
+}
+
+function solution2(n_str) {
+  const changedNumber = parseInt(n_str);
+
   return changedNumber;
 }
