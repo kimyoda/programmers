893f77ec71737eca38d2ba624b9d8b66c27c8465
kimyoda <rladygks1210@naver.com>
2025-03-17 15:58:47 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.js
@@ -7,3 +7,15 @@ function solution(a, b, flag) {
   isValues = flag ? a + b : a - b;
   return isValues;
 }
+
+function solution2(a, b, flag) {
+  let isValues = 0;
+  // flag가 true일때와 false일때  
+  // flag가 a + b일때와 a - b일대 나눠서 리턴
+  if (flag) {
+    isValues = a + b;
+  } else {
+    isValues = a - b;
+  }
+  return isValues;
+}
