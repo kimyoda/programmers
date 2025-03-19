8553fe69747822a3dce471f11791ee4a89a42517
kimyoda <rladygks1210@naver.com>
2025-03-19 22:40:03 +0900

    [fix] 리팩토링 및 solution 추가

--- 프로그래머스/0/181937. n의 배수/n의 배수.js
+++ 프로그래머스/0/181937. n의 배수/n의 배수.js
@@ -1,8 +1,22 @@
 function solution(num, n) {
   let result = 0;
-  
+
   if (num % n === 0) {
-    result = 1;      
+    result = 1;
   }
+  return result;
+}
+
+function solution2(num, n) {
+  // num 나누기 n이 0이면 1 아니면 0을 리턴
+  const result = num % n === 0 ? 1 : 0;
+
+  return result;
+}
+
+function solution3(num, n) {
+  // 비트 연산자
+  const result = (num % n === 0) | 0;
+
   return result;
 }
