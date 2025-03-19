46f5f1ed1dd578e607891d2c0bfe1b7ef5b14c45
kimyoda <rladygks1210@naver.com>
2025-03-19 21:45:00 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.js
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.js
@@ -1,8 +1,18 @@
 // 문자열 배열 arr
 // arr의 원소들을 순서대로 이어붙인 문자열
 function solution(arr) {
-    
-    const changedStrList = arr.join("");
-    
-    return changedStrList;
+  const changedStrList = arr.join("");
+
+  return changedStrList;
+}
+
+function solution2(arr) {
+  // for 문
+  let result = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+
+  return result;
 }
