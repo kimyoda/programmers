00868819bccedd04a32dae910f1d651794c4cbe9
kimyoda <rladygks1210@naver.com>
2025-03-23 14:12:25 +0900

    [fix] 리팩토링 및 solution2 추가

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,5 +1,11 @@
 function solution(my_string, k) {
   const strMultiply = my_string.repeat(k);
-    
+
+  return strMultiply;
+}
+
+function solution2(my_string, k) {
+  const strMultiply = Array(k).fill(my_string).join("");
+
   return strMultiply;
 }
