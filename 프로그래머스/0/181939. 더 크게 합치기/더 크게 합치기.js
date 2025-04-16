aa56aaf6169ca1928075d01c303dff4fc6aabe9e
kimyoda <rladygks1210@naver.com>
2025-04-16 23:47:32 +0900

    [feat] solution2 추가

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,6 +1,13 @@
 function solution(a, b) {
-  let a_b = Number(String(a) +  String(b));
+  let a_b = Number(String(a) + String(b));
   let b_a = Number(String(b) + String(a));
-  
+
+  return Math.max(a_b, b_a);
+}
+
+function solution2(a, b) {
+  // 템플릿 리터럴 사용하여 구하기
+  const a_b = Number(`${a}${b}`);
+  const b_a = Number(`${b}${a}`);
   return Math.max(a_b, b_a);
 }
