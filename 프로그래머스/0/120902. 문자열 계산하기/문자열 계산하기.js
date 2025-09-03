02a17a834158ab74d41299299f9947ec954eee06
KimYoHan <141495978+kimyoda@users.noreply.github.com>
2025-09-03 20:41:49 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -3,22 +3,25 @@
 // 공백이 없다. 0으로 시작하는 숫자는 없다.
 function solution(my_string) {
   // 1. 문자열을 공백으로 쪼개서 공백을 제외시킨다.
-  const arr = my_string.split(" ");
+//   const arr = my_string.split(" ");
+    
+//   // 2. reduce를 이용해 왼쪽부터 계산식을 수행한다.
+//   const result = arr.reduce((acc, cur, idx) => {
+//   // 3. 계산결과를 담을 변수
+//   let newResult = acc;
+//    // 4. cur이 연산자면, 그 다음 숫자를 확인해서 acc에 반영한다. +,- 연산자를 계산하여 저장한다.
+//     if (cur === "+") {
+//       newResult = acc + Number(arr[idx + 1]);
+//     } else if (cur === "-") {
+//       newResult = acc - Number(arr[idx + 1]);
+//     }
+//     // 숫자일 경우 그대로 저장한다.
+//     return newResult;
+//     // 초기화된 첫 번째는 숫자이다.
+//   }, Number(arr[0]));
+    
+//   return result;
+    
+  return Function(`return ${my_string}`)();
 
-  // 2. reduce를 이용해 왼쪽부터 계산식을 수행한다.
-  const result = arr.reduce((acc, cur, idx) => {
-    // 3. 누적 결과를 담을 변수 (acc에서 출발)
-    let currentTotal = acc;
-    // 4. cur이 연산자면, 그 다음 숫자를 확인해서 acc에 반영한다. +,- 연산자를 계산하여 저장한다.
-    if (cur === "+") {
-      currentTotal = acc + Number(arr[idx + 1]);
-    } else if (cur === "-") {
-      currentTotal = acc - Number(arr[idx + 1]);
-    }
-    // 5. cur이 숫자일 경우 acc 그대로 유지된다.
-    return currentTotal;
-    // 초기화된 첫 번째는 숫자를 정수로 변환한 값이다.
-  }, Number(arr[0]));
-
-  return result;
 }
